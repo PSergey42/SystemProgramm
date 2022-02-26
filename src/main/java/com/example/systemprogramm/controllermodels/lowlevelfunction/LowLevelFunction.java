ea6406ec08ff15b150ef47aa4a16602dded3697d
Sergey Polyakov <serega.uz@mail.ru>
2022-02-26 21:52:41 +0400

    3

--- src/main/java/com/example/systemprogramm/controllermodels/lowlevelfunction/LowLevelFunction.java
+++ src/main/java/com/example/systemprogramm/controllermodels/lowlevelfunction/LowLevelFunction.java
@@ -1,12 +1,15 @@
 package com.example.systemprogramm.controllermodels.lowlevelfunction;
 
 public class LowLevelFunction {
+    static{
+        System.loadLibrary("LowLevelFunction");
+    }
     public native static int div(int x, int y);    // импортируемая функция sum
 
     public native static int addWithOverflowCheck(int x, int y);
 
     public static void main(String[] args) {
-        System.loadLibrary("LowLevelFunction");        // загружаем библиотеку mydll.dll
-        System.out.println(div(2, 3));      // вызываем функцию
+        System.out.println(div(100, 2));      // вызываем функцию
+        System.out.println(addWithOverflowCheck(2147483645,2));
     }
 }
